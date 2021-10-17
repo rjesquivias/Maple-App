172c48761cddabf892046dfe55342fb7b325838a
RJ Esquivias <esquiviasrj@gmail.com>
2021-10-17 14:11:52 -0700

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Copyright 2021 RJ Esquivias. All Rights Reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,15 @@
+import Footer from "./Footer";
 import Header from "./Header";
 import NavCards from "./NavCards";
 
 const Homepage = () => {
     return (
         <>
+        <div className="wrapper">
         <Header />
         <NavCards />
+        <Footer />
+        </div>
         </>
     );
 };
