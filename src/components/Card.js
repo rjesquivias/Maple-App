4ce55d14215bfebe876cc81d72cc251c23174db9
RJ Esquivias <esquiviasrj@gmail.com>
2021-11-24 11:48:01 -0800

    Fix nav card link bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,9 @@ const Card = (props) => {
     return (
         <div className={'card'}>
             <img className="cover" src={props.image} alt={props.alt} />
-            <div className="card-image-text">
-                <Link to={props.path} >
-                    <h1>{props.title}</h1>
-                </Link>
-            </div>
+            <span className="card-image-text">
+                <h1>{props.title}</h1>
+            </span>
         </div>
     );
 };
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import Card from "./Card";
+import { Link } from "react-router-dom";
 
 const CardContainer = () => {
     const [className, setClassName] = useState("card-container")
 
     return (
         <div className={className} >
-            <Card image="img/bio-image.png" alt="Library with ladder in front" title="Bio" path="/bio" />
-            <Card image="img/gallery-image.png" alt="Gallery room with paintings on the walls" title="Gallery" path="/gallery" />
-            <Card image="img/breeder-image.png" alt="Hand holding a newborn puppy" title="Breeder" path="/breeder" />
+            <Link to={'/bio'} >
+                <Card image="img/bio-image.png" alt="Library with ladder in front" title="Bio" path="/bio" />
+            </Link>
+            <Link to={'/gallery'} >
+                <Card image="img/gallery-image.png" alt="Gallery room with paintings on the walls" title="Gallery" path="/gallery" />
+            </Link>
+            <Link to={'/breeder'} >
+                <Card image="img/breeder-image.png" alt="Hand holding a newborn puppy" title="Breeder" path="/breeder" />
+            </Link>
         </div>
     );
 };
